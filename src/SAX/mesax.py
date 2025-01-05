5c47c20ff9719eabbbd4e9348f1486b60503d7db
SullivanCastro <sullivan.castro@eleves.enpc.fr>
2025-01-05 21:46:33 +0100

    mesax update slopes

--- src/SAX/mesax.py
+++ src/SAX/mesax.py
@@ -3,23 +3,12 @@ from tqdm import tqdm
 from math import floor
 import sys
 
-def sliding_windows_partitions(data, windows_size, step_size):
+def sliding_windows_partitions(data, windows_size):
     """
     Split the data into windows of size windows_size. The windows are overlapping.
     The number of windows is num_windows.
     """
-    c_tilde = int(floor((len(data) - windows_size) / step_size)) + 1
-
-    processed_data = np.zeros((c_tilde, windows_size))
-    i = windows_size
-    s = 0
-
-    while i < len(data) + step_size:
-        processed_data[s] = data[i - windows_size:i]
-        i += step_size
-        s += 1
-
-    return processed_data
+    return data.reshape(data.shape[0]//windows_size, windows_size)
 
 
 def paa_aggregation(data, paa_size):
@@ -29,14 +18,12 @@ def paa_aggregation(data, paa_size):
     return data.reshape(data.shape[0], data.shape[1]//paa_size, paa_size)
 
 
-def synthetise_data(data):
+def synthetise_data(data, method):
     """
     Synthetise the data after the PAA aggregation.
     """
 
     synthetised_data = np.zeros((data.shape[0], data.shape[1], 3))
-
-    means = np.mean(data, axis=-1)
 
     argmins = np.argmin(data, axis=-1)
     mins = np.min(data, axis=-1)
@@ -44,10 +31,29 @@ def synthetise_data(data):
     argmaxs = np.argmax(data, axis=-1)
     maxs = np.max(data, axis=-1)
 
+    means = np.mean(data, axis=-1)
+    argmeans = data.shape[-1] // 2
 
-    synthetised_data[:, :, 0] = (argmins < argmaxs) * mins + (argmins > argmaxs) * maxs + (argmins == argmaxs) * means
-    synthetised_data[:, :, 1] = (argmins != argmaxs) * means + (argmins == argmaxs) * mins
-    synthetised_data[:, :, 2] = (argmins < argmaxs) * maxs + (argmins > argmaxs) * mins + (argmins == argmaxs) * maxs
+    if method=="slope":
+
+        synthetised_data[:, :, 0] = ((argmins < argmaxs) & (argmins <= argmeans)) * mins + \
+                                    ((argmaxs < argmins) & (argmaxs <= argmeans)) * maxs + \
+                                    ((argmeans < argmins) & (argmeans < argmaxs)) * means
+
+        synthetised_data[:, :, 1] = ((argmeans <= argmins) & (argmins < argmaxs) | (argmaxs < argmins) & (argmins <= argmeans)) * mins + \
+                                    ((argmeans <= argmaxs) & (argmaxs < argmins) | (argmins < argmaxs) & (argmaxs <= argmeans)) * maxs + \
+                                    ((argmaxs < argmeans) & (argmeans < argmins) | (argmins < argmeans) & (argmeans < argmaxs)) * means
+
+        synthetised_data[:, :, 2] = ((argmins > argmaxs) & (argmins >= argmeans)) * mins + \
+                                    ((argmaxs > argmins) & (argmaxs >= argmeans)) * maxs + \
+                                    ((argmeans > argmins) & (argmeans > argmaxs)) * means
+
+    elif method=="normal":
+        synthetised_data[:, :, 0] = (argmins <= argmaxs) * mins + (argmins >= argmaxs) * maxs + (argmins == argmaxs) * means
+        synthetised_data[:, :, 1] = (argmins != argmaxs) * means + (argmins == argmaxs) * mins
+        synthetised_data[:, :, 2] = (argmins <= argmaxs) * maxs + (argmins >= argmaxs) * mins + (argmins == argmaxs) * maxs
+    else:
+        raise ValueError("Invalid method for data synthetisation")
 
     return synthetised_data
 
@@ -58,7 +64,7 @@ def dyadic_alphabet(Q):
 
 class meSAX:
 
-    def __init__(self, K, windows_size, step_size, method="uniform"):
+    def __init__(self, K, windows_size, step_size, method="uniform", reconstruction_method="slope"):
         self.K = K
         self.method = method
         self.windows_size = windows_size
@@ -67,12 +73,13 @@ class meSAX:
         self.symbol = []
         self.alphabet = self._generate_alphabet()
         self.breakpoints = np.random.rand(2**self.K - 1)
+        self.reconstruction_method = reconstruction_method
 
 
     def _preprocess_data(self, data):
-        self.slided_data = sliding_windows_partitions(data, self.windows_size, self.step_size)
-        self.aggregated_data = paa_aggregation(self.slided_data, self.windows_size)
-        self.synthetised_data = synthetise_data(self.aggregated_data)
+        self.slided_data = sliding_windows_partitions(data, self.windows_size)
+        self.aggregated_data = paa_aggregation(self.slided_data, self.step_size)
+        self.synthetised_data = synthetise_data(self.aggregated_data, self.reconstruction_method)
         return self.synthetised_data
 
 
@@ -83,7 +90,7 @@ class meSAX:
                 output = ""
                 for x in self.synthetised_data[i, j]: # triplet element
                     b = 0
-                    while b < len(self.breakpoints) and x > self.breakpoints[b]:
+                    while b < len(self.alphabet) -1  and x > self.breakpoints[b]:
                         b += 1
                     output += str(self.alphabet[b])
                 self.symbol += output
@@ -122,13 +129,17 @@ class meSAX:
         self.reconstructed_data = np.zeros(len(symbol_list)*self.step_size)
         
         for i, triplet in tqdm(enumerate(symbol_list)):
-            mean = self.breakpoints[int(triplet[1],2)]
-            elem_0, elem_1 = self.breakpoints[int(triplet[0],2)], self.breakpoints[int(triplet[2],2)]
+            elem_0, elem_1, elem_2 = self.breakpoints[int(triplet[0],2)], self.breakpoints[int(triplet[1],2)], self.breakpoints[int(triplet[2],2)]
                 
-            min_, max_ = min(elem_0, elem_1), max(elem_0, elem_1)
-            if mean==min_==max_:
-                self.reconstructed_data[i*self.step_size:(i+1)*self.step_size] = mean * np.ones(self.step_size)
+
+            if self.reconstruction_method == "slope":
+                samples = np.zeros(self.step_size)
+                samples[:self.step_size//2] = np.linspace(elem_0, elem_1, self.step_size//2)            
+                samples[self.step_size//2:] = np.linspace(elem_1, elem_2, len(samples[self.step_size//2:]))
             else:
+                mean = elem_1
+                min_, max_ = min(elem_0, elem_2), max(elem_0, elem_2)
+                
                 samples = np.random.normal(mean, np.sqrt((max_-min_)/2), self.step_size)
                 while np.any(samples < min_) or np.any(samples > max_):
                     wrong_args_min = np.where(samples < min_)[0]
@@ -137,12 +148,12 @@ class meSAX:
                     samples[wrong_args] = np.random.normal(mean, 1, len(wrong_args))
                 
                 np.sort(samples)
-                if elem_0 > elem_1:
+                if elem_0 > elem_2:
                     samples = samples[::-1]
 
-                self.reconstructed_data[i*self.step_size:(i+1)*self.step_size] = samples
+            self.reconstructed_data[i*self.step_size:(i+1)*self.step_size] = samples
 
-        self.reconstructed_data = self.reconstructed_data[self.reconstructed_data != 0]
+        # self.reconstructed_data = self.reconstructed_data[self.reconstructed_data != 0]
         return self.reconstructed_data
     
     def _get_compression(self):
