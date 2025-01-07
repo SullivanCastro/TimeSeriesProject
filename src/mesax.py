263100a9904e95ec3509008a941c92d1aaada7be
sachabinder <binder.sacha@gmail.com>
2025-01-07 17:09:43 +0100

    WIP meSAX with rolling windows

---
+++ src/mesax.py
@@ -0,0 +1,167 @@
+import math
+import numpy as np
+
+from src.utils import sliding_window_on_last_axis, sliding_window_to_signal
+
+def dyadic_alphabet(alphabet_size) -> list[str]:
+    """Generate a dyadic alphabet of size Q = 2^L, where L is the smallest integer such that 2^L >= alphabet_size.
+    
+    Parameters:
+        alphabet_size: int: The size of the alphabet.
+
+    Returns:
+        alphabet: A list of binary strings representing the dyadic alphabet.
+    """
+    
+    level = math.ceil(math.log2(alphabet_size))
+
+    if not (alphabet_size & (alphabet_size - 1)) == 0:
+        print("The alphabet size must be a power of 2. The next power of 2 will be used.")
+        alphabet_size = 2**level
+        print(f"Alphabet size: {2**level}")
+
+    return [format(i, f'0{level}b') for i in range(alphabet_size)]
+
+
+class meSAX:
+    def __init__(self, alphabet_size, rolling_window_size, rolling_window_stride, paa_window_size):
+        self._alphabet_size = alphabet_size # K in the paper
+        self._rolling_window_size = rolling_window_size # w in the paper
+        self._rolling_window_stride = rolling_window_stride # s in the paper
+        self._paa_window_size = paa_window_size # R in the paper
+        self._alphabet = dyadic_alphabet(alphabet_size)
+        self._breakpoints_numbers = len(self._alphabet) - 1
+        self._breakpoints = None
+
+    def _paa(self, rolling_windows: np.ndarray) -> np.ndarray:
+        """
+        Extract features (min, average, max) from the data using PAA.
+        Map a window to a 3*R-dimensional vector.
+
+        Parameters:
+            rolling_windows: np.ndarray: A 2D time series data array. (c, self._rolling_window_size)
+            where c = (N - self._rolling_window_size) // self._rolling_window_stride + 1
+
+        Returns:
+        - paa_data: The PAA representation of the data.
+        """
+        # when stride = window_size, the sliding windows is not overlapped
+        paa_windows = sliding_window_on_last_axis(rolling_windows, self._paa_window_size, self._paa_window_size)
+
+        # retreive the min, mean and max of each last axis and their corresponding index
+        features_data = np.zeros((paa_windows.shape[0], paa_windows.shape[1], 3))
+
+        argmins = np.argmin(paa_windows, axis=-1)
+        mins = np.min(paa_windows, axis=-1)
+
+        argmaxs = np.argmax(paa_windows, axis=-1)
+        maxs = np.max(paa_windows, axis=-1)
+
+        means = np.mean(paa_windows, axis=-1)
+        argmeans = (argmins + argmaxs) // 2
+
+        # order the features min, mean, max by appearing time
+        features_data[:, :, 0] = np.where(
+            (argmins < argmaxs) & (argmins < argmeans),
+            mins,
+            np.where(
+                (argmaxs < argmins) & (argmaxs < argmeans),
+                maxs,
+                means
+            )
+        )
+
+        features_data[:, :, 1] = np.where(
+            (argmeans < argmins) & (argmins < argmaxs) | (argmaxs < argmins) & (argmins < argmeans),
+            mins,
+            np.where(
+                (argmeans < argmaxs) & (argmaxs < argmins) | (argmins < argmaxs) & (argmaxs < argmeans),
+                maxs,
+                means
+            )
+        )
+
+        features_data[:, :, 2] = np.where(
+            (argmins > argmaxs) & (argmins > argmeans),
+            mins,
+            np.where(
+                (argmaxs > argmins) & (argmaxs > argmeans),
+                maxs,
+                means
+            )
+        )
+
+        return features_data
+
+    def fit(self, X: np.ndarray, fitting_method = "uniform") -> None:
+        """
+        Fit the meSAX model to the data by learning the breakpoints.
+
+        Parameters:
+            data: np.ndarray: A 1D time series data array.
+            fitting_method: str: The method used to generate the breakpoints. Default is "uniform".
+        """
+        if fitting_method == "uniform":
+            self._breakpoints = np.linspace(X.min(), X.max(), self._breakpoints_numbers)
+        elif fitting_method == "median":
+            self._breakpoints = np.quantile(X, np.linspace(0, 1, self._breakpoints_numbers))
+        else:
+            raise NotImplementedError("Invalid method for breakpoints generation")
+        
+        return self
+    
+    def transform(self, data: np.ndarray) -> np.ndarray:
+        """
+        Transform the data into 
+
+        Parameters:
+            data: np.ndarray: A 1D time series data array. (N, )
+
+        Returns:
+        - sax_data: The SAX representation of the data.
+        """
+        rolling_windows = sliding_window_on_last_axis(
+            series=data,
+            window_size=self._rolling_window_size,
+            stride=self._rolling_window_stride
+        )
+
+        triplets_data = self._paa(rolling_windows)
+        
+        return triplets_data
+    
+    def predict(self, triplets_data: np.ndarray, sampling_method = "slope") -> np.ndarray:
+        """
+        Reconstruct the time series data from the SAX representation.
+
+        Parameters:
+            data: np.ndarray: A 1D time series data array.
+
+        Returns:
+        - reconstructed_data: The reconstructed time series data from the SAX representation.
+        """
+
+        if sampling_method == "slope":
+            
+            mid_point = self._paa_window_size // 2
+
+            theta_1 = triplets_data[..., 0][..., np.newaxis]
+            theta_2 = triplets_data[..., 1][..., np.newaxis]
+            theta_3 = triplets_data[..., 2][..., np.newaxis]
+
+            x = np.arange(self._paa_window_size)
+
+            reconstructed_paa_signals = (
+                    (theta_1 + (theta_2 - theta_1) / mid_point * x) * (x < mid_point) # slope 1
+                    + (theta_2 + (theta_3 - theta_2) / mid_point * x) * (x >= mid_point) # slope 2
+                )
+            reconstructed_rolling_window = reconstructed_paa_signals.reshape(reconstructed_paa_signals.shape[0], -1)
+
+        else:
+            raise NotImplementedError("Invalid method for reconstruction")
+            
+        return sliding_window_to_signal(reconstructed_rolling_window, self._rolling_window_stride)
+
+
+
+            
--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,30 @@
 import numpy as np
 
 
-def signal_to_sliding_window(series: np.ndarray, window_size: int, stride:int) -> np.ndarray:
+def sliding_window_on_last_axis(series: np.ndarray, window_size: int, stride:int) -> np.ndarray:
     """
-    Create a 2D array of sliding windows from a temporal series.
+    Create a n+1 D array of sliding windows from a temporal series.
     
     Parameters:
-        series (np.ndarray): 1D array representing the temporal series.
+        series (np.ndarray): 1D array representing the temporal series. (..., N,)
         window_size (int): Size of each sliding window.
         stride (int): Step size between consecutive windows.
     
     Returns:
-        np.ndarray: 2D array where each row is a sliding window.
+        np.ndarray: 2D array where each row is a sliding window. (..., C, window_size)
+        where C = (N - window_size) // stride + 1
     """
     
-    return np.array([
-        series[i : i + window_size]
-        for i in range(0, len(series) - window_size + 1, stride)
-    ])
+    # Compute the number of sliding windows
+    last_dim = series.shape[-1]
+    num_windows = (last_dim - window_size) // stride + 1
+    
+    # Create sliding windows using advanced NumPy slicing
+    shape = series.shape[:-1] + (num_windows, window_size)
+    strides = series.strides[:-1] + (series.strides[-1] * stride, series.strides[-1])
+    sliding_windows = np.lib.stride_tricks.as_strided(series, shape=shape, strides=strides)
+    
+    return sliding_windows
 
 
 def sliding_window_to_signal(windows: np.ndarray, stride: int) -> np.ndarray:
